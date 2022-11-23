35bd2ceee7f4cfb8bbdc30028fbc12b5d5022f59
Pierre Le Fevre <hello@pierrelf.com>
2022-11-23 17:40:27 +0100

    Co-authored-by: Emil Karlsson <own.emka@gmail.com>

--- ID2222 Data Mining/hw3/main-uni.py
+++ ID2222 Data Mining/hw3/main-uni.py
@@ -10,8 +10,9 @@ import time
 
 def count_wedges(edge_t, edge_res):
     # print("     ==> Counting wedges")
-    out_degree = {}
-    in_degree = {}
+    # out_degree = {}
+    # in_degree = {}
+    degree = {}
     nodes = set()
 
     edge_t_from = edge_t[0]
@@ -19,7 +20,7 @@ def count_wedges(edge_t, edge_res):
 
     new_wedges = set()
 
-    for edge in edge_res:
+    for edge in set(edge_res):
         if edge:
             from_node, to_node = edge
 
@@ -27,23 +28,42 @@ def count_wedges(edge_t, edge_res):
             nodes.add(from_node)
             nodes.add(to_node)
 
-            # Save wedges that contain edge_t
-            if from_node == edge_t_to:
-                # edge_t -> shared -> other
+            # Undirected test
+            if edge_t_from == from_node:
+                new_wedges.add((edge_t_to, from_node, to_node))
+            elif edge_t_from == to_node:
+                new_wedges.add((edge_t_to, to_node, from_node))
+            elif edge_t_to == from_node:
                 new_wedges.add((edge_t_from, from_node, to_node))
-            if to_node == edge_t_from:
-                # other -> shared -> edge_t
-                new_wedges.add((from_node, to_node, edge_t_to))
+            elif edge_t_to == to_node:
+                new_wedges.add((edge_t_from, to_node, from_node))
+
+            # # Save wedges that contain edge_t
+            # if from_node == edge_t_to:
+            #     # edge_t -> shared -> other
+            #     new_wedges.append((edge_t_from, from_node, to_node))
+            # if to_node == edge_t_from:
+            #     # other -> shared -> edge_t
+            #     new_wedges.append((from_node, to_node, edge_t_to))
 
             # Count wedges
-            out_degree[from_node] = out_degree.get(from_node, 0) + 1
-            in_degree[to_node] = in_degree.get(to_node, 0) + 1
+            # out_degree[from_node] = out_degree.get(from_node, 0) + 1
+            # in_degree[to_node] = in_degree.get(to_node, 0) + 1
+            degree[from_node] = degree.get(from_node, 0) + 1
+            degree[to_node] = degree.get(to_node, 0) + 1
 
     tot_wedges = 0
     for node in nodes:
         # If the node contains both in and out, it must have at least one wedge
-        if node in in_degree and node in out_degree:
-            tot_wedges += in_degree[node] * out_degree[node]
+        if node in degree:
+
+            # Undirected test
+            n = degree[node]
+            tot_wedges += n * (n-1) / 2
+
+            # tot_wedges += in_degree[node] * out_degree[node]
+
+
 
     return new_wedges, tot_wedges
 
@@ -52,18 +72,13 @@ def main():
     print('=== Mining data streams ===')
 
     file = open('datasets/web-Stanford.txt', 'r')
-    lines = file.readlines()
-    random.shuffle(lines)
-    shuffled_lines = lines
-
-    print(f'Found {len(shuffled_lines)} lines')
 
     # Uniform reservoir of edges
-    s_edges = 10000
+    s_edges = 1000
     edge_res = [None] * s_edges
 
     # Wedges seen in edge reservoir
-    s_wedges = 10000
+    s_wedges = 1000
     wedge_res = [None] * s_wedges
 
     # is_closed[i] undicates whether wedge_res[i] is closed
@@ -81,22 +96,16 @@ def main():
 
     t = 0
     t_updated = 0
-    counter = 0
     while True:
         time_start = time.time()
 
         # Get next line
-        if t == len(shuffled_lines):
+        line = file.readline()
+        if not line:
             break
-        line = shuffled_lines[t]
-
-        # line = file.readline()
-        # if not line:
-        #     break
 
         # Skip commented rows
         if line[0] == '#':
-            t += 1
             continue
 
         # Sanitize
@@ -105,7 +114,6 @@ def main():
 
         # Skip self loops
         if len(set(edge_t)) < 2:
-            t += 1
             continue
 
         t += 1
@@ -120,20 +128,23 @@ def main():
                 wedge_from, _, wedge_to = wedge
                 edge_t_from, edge_t_to = edge_t
 
-                if edge_t_from == wedge_to and edge_t_to == wedge_from:
+                # Undirected test
+                edge_set = set([wedge_from, wedge_to, edge_t_from, edge_t_to])
+                if len(edge_set) == 2:
                     is_closed[i] = True
+
+                # if edge_t_from == wedge_to and edge_t_to == wedge_from:
+                #     is_closed[i] = True
 
         time_triangles += time.time()-time_start
         time_start = time.time()
 
         # print(" ==> Update elements in edge reservoir using probability")
         # Update elements in edge reservoir using probability
-
         edges_updated = False
         for i in range(s_edges):
             # If random number is between 0 and k, replace the node at that place
             if random.uniform(0, 1) <= 1 / t:
-                counter += 1
                 edge_res[i] = edge_t
                 edges_updated = True
 
@@ -159,15 +170,13 @@ def main():
 
             time_write_wedges += time.time() - time_start
 
-        unique_edges = len(set(edge_res)) / s_edges
-        
         p = is_closed.count(True)/s_wedges
         k_t = 3*p
         T_t = (p*(t**2))/(s_edges*(s_edges-1)) * tot_wedges
 
-        if t % 500 == 0:
-            print(f'Time {t}; Triangles {round(T_t)}; Transitivity {k_t:.4f} Updated {counter}; Unique {unique_edges}; Init {time_init/t:.4f}, Triangles{time_triangles/t:.4f}, Edges{time_new_edge/t:.4f}, Wedges{time_count_wedges/t_updated:.4f}, Write{time_write_wedges/t_updated:.4f}', end='\r')
-            counter = 0
+        if t % 1000 == 0:
+            print(f'Time {t}; Triangles {round(T_t)}; Transitivity {k_t:.4f}; Init {time_init/t:.4f}, Triangles{time_triangles/t:.4f}, Edges{time_new_edge/t:.4f}, Wedges{time_count_wedges/t_updated:.4f}, Write{time_write_wedges/t_updated:.4f}', end='\r')
+
     print(f'Time {t}; Triangles {round(T_t)}; Transitivity {k_t:.4f}%')
 
 
