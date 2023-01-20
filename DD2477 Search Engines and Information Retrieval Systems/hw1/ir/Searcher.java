6b079103a588d2a9e1a73a4444019e6ce9f0b492
Emil Karlsson <own.emka@gmail.com>
2023-01-20 15:23:53 +0100

    fix dd2477 hw1 1.3

--- DD2477 Search Engines and Information Retrieval Systems/hw1/ir/Searcher.java
+++ DD2477 Search Engines and Information Retrieval Systems/hw1/ir/Searcher.java
@@ -38,57 +38,69 @@ public class Searcher {
 
         switch (queryType) {
             case INTERSECTION_QUERY:
-                break;
+                return intersectionQuery(query);
             case PHRASE_QUERY:
-                var queryPostingsList = new ArrayList<PostingsList>();
-
-                // step 1: collect all posting list for individual terms
-                for (var queryTerm : query.queryterm) {
-                    queryPostingsList.add(index.getPostings(queryTerm.term));
-                }
-
-                if (queryPostingsList.size() == 0) {
-                    return null;
-                }
-
-                // step 2: intersection algorithm with rest
-
-                var totalAnswer = new PostingsList();
-
-                for (int i = 0; i < queryPostingsList.get(0).size(); i++) {
-                    totalAnswer.add(queryPostingsList.get(0).get(i));
-                }
-
-                for (int queryListIndex = 1; queryListIndex < queryPostingsList.size(); queryListIndex++) {
-                    var queryList = queryPostingsList.get(queryListIndex);
-
-                    int i = 0, j = 0;
-
-                    var answer = new PostingsList();
-                    while (i < totalAnswer.size() && j < queryList.size()) {
-                        var entry1 = totalAnswer.get(i);
-                        var entry2 = queryList.get(j);
-
-                        if (entry1.docID == entry2.docID) {
-                            answer.add(entry1);
-                            i++;
-                            j++;
-                        } else if (entry1.docID < entry2.docID) {
-                            i++;
-                        } else {
-                            j++;
-                        }
-                    }
-                    totalAnswer = answer;
-                }
-
-                return totalAnswer;
+                return phraseQuery(query);
             case RANKED_QUERY:
-                break;
+                return rankedQuery(query);
             default:
                 break;
         }
 
+        return null;
+    }
+
+    private PostingsList intersectionQuery(Query query) {
+        var queryPostingsList = new ArrayList<PostingsList>();
+
+        // step 1: collect all posting list for individual terms
+        for (var queryTerm : query.queryterm) {
+            queryPostingsList.add(index.getPostings(queryTerm.term));
+        }
+
+        if (queryPostingsList.size() == 0) {
+            return null;
+        }
+
+        // step 2: intersection algorithm with rest
+
+        var totalAnswer = new PostingsList();
+
+        for (int i = 0; i < queryPostingsList.get(0).size(); i++) {
+            totalAnswer.add(queryPostingsList.get(0).get(i));
+        }
+
+        for (int queryListIndex = 1; queryListIndex < queryPostingsList.size(); queryListIndex++) {
+            var queryList = queryPostingsList.get(queryListIndex);
+
+            int i = 0, j = 0;
+
+            var answer = new PostingsList();
+            while (i < totalAnswer.size() && j < queryList.size()) {
+                var entry1 = totalAnswer.get(i);
+                var entry2 = queryList.get(j);
+
+                if (entry1.docID == entry2.docID) {
+                    answer.add(entry1);
+                    i++;
+                    j++;
+                } else if (entry1.docID < entry2.docID) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+            totalAnswer = answer;
+        }
+
+        return totalAnswer;
+    }
+
+    private PostingsList phraseQuery(Query query) {
+        return null;
+    }
+
+    private PostingsList rankedQuery(Query query) {
         return null;
     }
 }
