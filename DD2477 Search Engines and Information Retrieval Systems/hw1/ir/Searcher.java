d0324b37990cdad74881f97810238e58126d832b
Emil Karlsson <own.emka@gmail.com>
2023-01-27 09:53:08 +0100

    dd2477 hw1 add skip pointers to both queries

--- DD2477 Search Engines and Information Retrieval Systems/hw1/ir/Searcher.java
+++ DD2477 Search Engines and Information Retrieval Systems/hw1/ir/Searcher.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -30,6 +31,9 @@ public class Searcher {
         this.kgIndex = kgIndex;
     }
 
+    int noSkips = 0;
+    int skipDistance = 0;
+
     /**
      * Searches the index for postings matching the query.
      * 
@@ -37,7 +41,8 @@ public class Searcher {
      */
     public PostingsList search(Query query, QueryType queryType, RankingType rankingType, NormalizationType normType) {
 
-        System.out.println("query term: " + Arrays.toString(query.queryterm.toArray()));
+        noSkips = 0;
+        skipDistance = 0;
 
         switch (queryType) {
             case INTERSECTION_QUERY:
@@ -57,7 +62,6 @@ public class Searcher {
         var queryPostingsList = new ArrayList<PostingsList>();
 
         // step 1: collect all posting list for individual terms
-
         var queryTermSet = new HashSet<String>();
         for (var queryTerm : query.queryterm) {
             queryTermSet.add(queryTerm.term);
@@ -77,24 +81,7 @@ public class Searcher {
         for (int queryListIndex = 1; queryListIndex < queryPostingsList.size(); queryListIndex++) {
             var queryList = queryPostingsList.get(queryListIndex);
 
-            int i = 0, j = 0;
-
-            var answer = new PostingsList(queryList.getToken());
-            while (i < totalAnswer.size() && j < queryList.size()) {
-                var entry1 = totalAnswer.get(i);
-                var entry2 = queryList.get(j);
-
-                if (entry1.docID == entry2.docID) {
-                    answer.add(entry1);
-                    i++;
-                    j++;
-                } else if (entry1.docID < entry2.docID) {
-                    i++;
-                } else {
-                    j++;
-                }
-            }
-            totalAnswer = answer;
+            totalAnswer = intersectComparison(totalAnswer, queryList);
 
             if (totalAnswer.size() == 0) {
                 break;
@@ -104,127 +91,36 @@ public class Searcher {
         return totalAnswer;
     }
 
+    private PostingsList intersectComparison(PostingsList p1, PostingsList p2) {
+        var answer = makeIntersectionQuery(p1, p2, (entry1, entry2) -> {
+            return entry1;
+        });
+
+        return answer;
+    }
+
     private PostingsList phraseQuery(Query query) {
-        var queryPostingsMap = new HashMap<String, PostingsList>();
-        var queryPostingsList = new ArrayList<PostingsList>();
+        var cache = new HashMap<String, PostingsList>();
 
-        // step 1: collect all posting list for individual terms
-        for (var queryTerm : query.queryterm) {
-            var term = queryTerm.term;
-
-            if (queryPostingsMap.containsKey(term)) {
-                queryPostingsList.add(queryPostingsMap.get(term));
-            } else {
-                var newList = index.getPostings(term);
-                queryPostingsMap.put(term, newList);
-                queryPostingsList.add(newList);
-            }
-        }
-
-        if (queryPostingsList.size() == 0) {
+        if (query.size() == 0) {
             return null;
         }
 
-        // step 2: modified intersection algorithm with the rest
-        var totalAnswer = new PostingsList(queryPostingsList.get(0));
+        var intialPostingsList = index.getPostings(query.queryterm.get(0).term);
+        var totalAnswer = new PostingsList(intialPostingsList);
 
         var start = System.currentTimeMillis();
 
-        int noSkips = 0;
-        int skipDistance = 0;
-        
-        for (int queryListIndex = 1; queryListIndex < queryPostingsList.size(); queryListIndex++) {
-            var queryList = queryPostingsList.get(queryListIndex);
+        for (int i = 0; i < query.size(); i++) {
+            var token = query.queryterm.get(i).term;
 
-            var totalAnswerSkipInterval = (int) Math.sqrt(totalAnswer.size());
-            var queryListSkipInterval = (int) Math.sqrt(queryList.size());
-
-            int i = 0, j = 0;
-
-
-            var answer = new PostingsList(queryList.getToken());
-            while (i < totalAnswer.size() && j < queryList.size()) {
-                var entry1 = totalAnswer.get(i);
-                var entry2 = queryList.get(j);
-                var combined = new PostingsEntry(entry1.docID, 0);
-
-                var entry1SkipInterval = (int) Math.sqrt(entry1.offsets.size());
-                var entry2SkipInterval = (int) Math.sqrt(entry2.offsets.size());
-
-                if (entry1.docID == entry2.docID) {
-
-                    for (int e1 = 0; e1 < entry1.offsets.size(); e1++) {
-                        for (int e2 = 0; e2 < entry2.offsets.size(); e2++) {
-                            // check if we can use some skips
-                            if (e1 % entry1SkipInterval == 0) {
-                                var skipTo = Math.min(e1 + entry1SkipInterval, entry1.offsets.size() - 1);
-                                var offset1Skip = entry1.offsets.get(skipTo);
-                                if (offset1Skip < entry2.offsets.get(e2)) {
-                                    e1 = skipTo;
-
-                                    noSkips++;
-                                    skipDistance += entry1SkipInterval;
-                                }
-                            }
-
-                            // check if we can use some skips
-                            if (e2 % entry2SkipInterval == 0) {
-                                var skipTo = Math.min(e2 + entry2SkipInterval, entry2.offsets.size() - 1);
-                                var offset2Skip = entry2.offsets.get(skipTo);
-                                if (offset2Skip <= entry1.offsets.get(e1)) {
-                                    e2 = skipTo;
-
-                                    noSkips++;
-                                    skipDistance += entry2SkipInterval;
-                                }
-                            }
-
-                            var offset1 = entry1.offsets.get(e1);
-                            var offset2 = entry2.offsets.get(e2);
-
-                            if (offset2 == offset1 + 1) {
-                                combined.offsets.add(offset2);
-                            }
-                        }
-                    }
-
-                    if (combined.offsets.size() != 0) {
-                        answer.add(combined);
-                    }
-
-                    i++;
-                    j++;
-                } else if (entry1.docID < entry2.docID) {
-                    // check if we can use some skips
-                    if (i % totalAnswerSkipInterval == 0) {
-                        var skipTo = Math.min(i + totalAnswerSkipInterval, totalAnswer.size() - 1);
-                        var entry1Skip = totalAnswer.get(skipTo);
-                        if (entry1Skip.docID <= entry2.docID) {
-                            i = skipTo;
-                        } else {
-                            i++;
-                        }
-                    } else {
-                        i++;
-                    }
-
-                } else {
-                    // check if we can use some skips
-                    if (j % queryListSkipInterval == 0) {
-                        var skipTo = Math.min(j + totalAnswerSkipInterval, queryList.size() - 1);
-                        var entry2Skip = queryList.get(skipTo);
-                        if (entry2Skip.docID <= entry1.docID) {
-                            j = skipTo;
-                        } else {
-                            j++;
-                        }
-                    } else {
-                        j++;
-                    }
-
-                }
+            var cachedList = cache.get(token);
+            if (cachedList == null) {
+                cachedList = index.getPostings(token);
+                cache.put(token, cachedList);
             }
-            totalAnswer = answer;
+
+            totalAnswer = phraseComparison(totalAnswer, cachedList);
 
             if (totalAnswer.size() == 0) {
                 break;
@@ -239,6 +135,126 @@ public class Searcher {
         System.out.println("phrase query took: " + (end - start) + " ms");
 
         return totalAnswer;
+    }
+
+    private PostingsList phraseComparison(PostingsList p1, PostingsList p2) {
+        var answer = makeIntersectionQuery(p1, p2, (entry1, entry2) -> {
+            var combined = new PostingsEntry(entry1.docID, 0);
+
+            var entry1SkipInterval = (int) Math.sqrt(entry1.offsets.size());
+            var entry2SkipInterval = (int) Math.sqrt(entry2.offsets.size());
+
+            for (int e1 = 0; e1 < entry1.offsets.size(); e1++) {
+                for (int e2 = 0; e2 < entry2.offsets.size(); e2++) {
+                    if (canSkipOffsets(e1, entry1SkipInterval, entry2.offsets.get(e2), entry1.offsets)) {
+                        e1 += entry1SkipInterval;
+
+                        noSkips++;
+                        skipDistance += entry1SkipInterval;
+                    }
+
+                    if (canSkipOffsets(e2, entry2SkipInterval, entry1.offsets.get(e1), entry2.offsets)) {
+                        e2 += entry2SkipInterval;
+
+                        noSkips++;
+                        skipDistance += entry2SkipInterval;
+                    }
+
+                    var offset1 = entry1.offsets.get(e1);
+                    var offset2 = entry2.offsets.get(e2);
+
+                    if (offset2 == offset1 + 1) {
+                        combined.offsets.add(offset2);
+                    }
+                }
+            }
+
+            if (combined.offsets.size() != 0) {
+                return combined;
+            }
+
+            return null;
+        });
+
+        return answer;
+    }
+
+    private interface IntersectHandler {
+        public PostingsEntry onMatch(PostingsEntry entry1, PostingsEntry entry2);
+    }
+
+    private PostingsList makeIntersectionQuery(PostingsList p1, PostingsList p2, IntersectHandler handler) {
+        var answer = new PostingsList(p2.getToken());
+
+        var p1SkipInterval = (int) Math.sqrt(p1.size());
+        var p2SkipInterval = (int) Math.sqrt(p2.size());
+
+        int i = 0, j = 0;
+        while (i < p1.size() && j < p2.size()) {
+            var entry1 = p1.get(i);
+            var entry2 = p2.get(j);
+
+            if (entry1.docID == entry2.docID) {
+
+                var result = handler.onMatch(entry1, entry2);
+                if (result != null) {
+                    answer.add(result);
+                }
+
+                i++;
+                j++;
+            } else if (entry1.docID < entry2.docID) {
+                if (canSkipDocuments(i, p1SkipInterval, entry1.docID, p1)) {
+                    i += p1SkipInterval;
+                } else {
+                    i++;
+                }
+            } else {
+                if (canSkipDocuments(j, p2SkipInterval, entry1.docID, p2)) {
+                    j += p2SkipInterval;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean canSkipOffsets(int index, int interval, int otherOffset, List<Integer> offsets) {
+        if (index % interval != 0) {
+            return false;
+        }
+
+        var skipTo = index + interval;
+        if (skipTo >= offsets.size()) {
+            return false;
+        }
+
+        var offset1Skip = offsets.get(skipTo);
+        if (offset1Skip >= otherOffset) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean canSkipDocuments(int index, int interval, int otherDocId, PostingsList postingsList) {
+        if (index % interval != 0) {
+            return false;
+        }
+
+        var skipTo = index + interval;
+        if (skipTo >= postingsList.size()) {
+            return false;
+        }
+
+        var entry2Skip = postingsList.get(skipTo);
+        if (entry2Skip.docID > otherDocId) {
+            return false;
+        }
+
+        return true;
     }
 
     private PostingsList rankedQuery(Query query) {
